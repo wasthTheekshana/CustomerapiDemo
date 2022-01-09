adf6720817d7e6cc758239c1129c7205a2928f55
Theekshana <47950733+wasthTheekshana@users.noreply.github.com>
2022-01-09 23:59:26 +0530

    Initial commit

---
+++ src/main/java/com/wasathDev/customerapiDemo/CustomerapiDemoApplication.java
@@ -0,0 +1,13 @@
+package com.wasathDev.customerapiDemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CustomerapiDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CustomerapiDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/wasathDev/customerapiDemo/CustomerapiDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wasathDev.customerapiDemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CustomerapiDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
