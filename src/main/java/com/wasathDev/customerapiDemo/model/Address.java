01a8e00f16c1ededce42cfba3f9a862d0929366c
Theekshana <47950733+wasthTheekshana@users.noreply.github.com>
2022-01-18 10:49:20 +0530

    add cutomer codes

--- src/main/java/com/wasathDev/customerapiDemo/model/Address.java
+++ src/main/java/com/wasathDev/customerapiDemo/model/Address.java
@@ -1,4 +1,6 @@
 package com.wasathDev.customerapiDemo.model;
 
 public class Address {
+
+//    streetName,city,zipcode,country
 }
--- src/main/java/com/wasathDev/customerapiDemo/model/Customer.java
+++ src/main/java/com/wasathDev/customerapiDemo/model/Customer.java
@@ -1,18 +1,28 @@
 package com.wasathDev.customerapiDemo.model;
 
+import lombok.Data;
+import org.springframework.format.annotation.DateTimeFormat;
+
 import javax.persistence.*;
 import java.time.LocalDate;
 
 @Entity
 @Table(name="V_Customer")
+@Data
 public class Customer {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "Customer_Id")
     private long customerId;
+    @Column(name = "Name", nullable = false,length = 70)
     private FullName name;
+    @Column(name = "Email", nullable = false,length = 100)
     private String email;
+    @Column(name = "Mobile_No")
     private long mobileNo;
+    @DateTimeFormat(iso = DateTimeFormat.ISO.DATE)
+    @Column(name = "dob")
     private LocalDate dob;
 
 
